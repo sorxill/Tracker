297abf9c2f1241c5100c3c2394ace5d565b15ccf
suredrope <karim.kushaliev@mail.ru>
2024-01-05 19:14:40 +0300

    refactor: move to the modern style of creating models
    In SQLalchemy 2.x `mapped_column` supersedes `Column`

---
+++ src/db/models.py
@@ -0,0 +1,21 @@
+import uuid
+
+from sqlalchemy import Boolean, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    surname: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
