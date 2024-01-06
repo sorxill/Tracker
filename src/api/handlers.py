1055683f3e1dbc5b5826d9232e2e5de7da29c208
suredrope <karim.kushaliev@mail.ru>
2024-01-06 21:48:35 +0300

    feat: map user's crud operations to endpoints (except for update)

--- src/api/handlers.py
+++ src/api/handlers.py
@@ -1,10 +1,15 @@
 from logging import getLogger
+from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api.actions.user.user_crud import create_new_user
+from src.api.actions.user.user_crud import (
+    create_new_user,
+    read_user_by_email,
+    read_user_by_id,
+)
 from src.api.schemas import ShowUser, UserCreate
 from src.db.session import get_db
 
@@ -20,3 +25,29 @@ async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)) -> S
     except IntegrityError as err:
         logger.error(err)
         raise HTTPException(status_code=503, detail=f"Database error: {err}") from err
+
+
+@user_router.get("/read_by_id", response_model=ShowUser)
+async def get_user_by_id(
+    user_id: UUID,
+    db: AsyncSession = Depends(get_db),
+) -> ShowUser:
+    user = await read_user_by_id(user_id, db)
+    if user is None:
+        raise HTTPException(
+            status_code=404, detail=f"User with id {user_id} not found."
+        )
+    return user
+
+
+@user_router.get("/get_by_email", response_model=ShowUser)
+async def get_user_by_email(
+    email: str,
+    db: AsyncSession = Depends(get_db),
+) -> ShowUser:
+    user = await read_user_by_email(email, db)
+    if user is None:
+        raise HTTPException(
+            status_code=404, detail=f"User with email {email} not found."
+        )
+    return user
