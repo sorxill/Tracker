ad44279e404e955dc4d5fc06e757391acd5f40b8
suredrope <karim.kushaliev@mail.ru>
2024-01-06 21:48:00 +0300

    feat: add CRUD operations to user

--- src/api/actions/user/user_crud.py
+++ src/api/actions/user/user_crud.py
@@ -1,8 +1,13 @@
+from uuid import UUID
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.api.schemas import ShowUser, UserCreate
 from src.db.dals.user_dal import UserDAL
+from src.db.models.user_model import User
 
 
-async def create_new_user(body: UserCreate, session) -> ShowUser:
+async def create_new_user(body: UserCreate, session: AsyncSession) -> ShowUser:
     async with session.begin():
         user_dal = UserDAL(session)
         user = await user_dal.create_user(
@@ -17,3 +22,39 @@ async def create_new_user(body: UserCreate, session) -> ShowUser:
             email=user.email,
             is_active=user.is_active,
         )
+
+
+async def read_user_by_email(email: str, session: AsyncSession) -> User | None:
+    async with session.begin():
+        user_dal = UserDAL(session)
+        user = await user_dal.get_user_by_email(email)
+        if user is not None:
+            return user
+
+
+async def read_user_by_id(user_id, session) -> User | None:
+    async with session.begin():
+        user_dal = UserDAL(session)
+        user = await user_dal.get_user_by_id(
+            user_id=user_id,
+        )
+        if user is not None:
+            return user
+
+
+async def delete_user(user_id, session) -> UUID | None:
+    async with session.begin():
+        user_dal = UserDAL(session)
+        deleted_user_id = await user_dal.delete_user(
+            user_id=user_id,
+        )
+        return deleted_user_id
+
+
+async def update_user(updated_user_params: dict, user_id: UUID, session) -> UUID | None:
+    async with session.begin():
+        user_dal = UserDAL(session)
+        updated_user_id = await user_dal.update_user(
+            user_id=user_id, **updated_user_params
+        )
+        return updated_user_id
