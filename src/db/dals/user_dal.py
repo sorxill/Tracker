1304996630731a5d991932571a906c0e824add3e
suredrope <karim.kushaliev@mail.ru>
2024-01-06 21:50:32 +0300

    feat: add CRUD operations in UserDAL

--- src/db/dals/user_dal.py
+++ src/db/dals/user_dal.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from sqlalchemy import and_, select, update
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.db.models.user_model import User
@@ -27,18 +27,24 @@ class UserDAL:
         await self.db_session.flush()
         return new_user
 
-    async def get_user_by_email(self, email: str) -> UUID | None:
+    async def get_user_by_email(self, email: str) -> User | None:
         query = select(User).where(User.email == email)
         res = await self.db_session.execute(query)
         user_row = res.fetchone()
         if user_row is not None:
             return user_row[0]
-        return None
+
+    async def get_user_by_id(self, user_id: UUID) -> User | None:
+        query = select(User).where(User.user_id == user_id)
+        res = await self.db_session.execute(query)
+        user_row = res.fetchone()
+        if user_row is not None:
+            return user_row[0]
 
     async def update_user(self, user_id: UUID, **kwargs) -> UUID | None:
         query = (
             update(User)
-            .where(and_(User.user_id == user_id, User.is_active))
+            .where(User.user_id == user_id, User.is_active)
             .values(kwargs)
             .returning(User.user_id)
         )
@@ -46,4 +52,15 @@ class UserDAL:
         update_user_id_row = res.fetchone()
         if update_user_id_row is not None:
             return update_user_id_row[0]
-        return None
+
+    async def delete_user(self, user_id: UUID) -> UUID | None:
+        query = (
+            update(User)
+            .where(User.user_id == user_id, User.is_active)
+            .values(is_active=False)
+            .returning(User.user_id)
+        )
+        res = await self.db_session.execute(query)
+        deleted_user_id_row = res.fetchone()
+        if deleted_user_id_row is not None:
+            return deleted_user_id_row[0]
