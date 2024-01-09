a841e81c89819fd0f81201433e16a7759d9c4ec5
suredrope <karim.kushaliev@mail.ru>
2024-01-09 17:49:03 +0300

    refactor: remove ProjectBase

--- src/api/schemas/project.py
+++ src/api/schemas/project.py
@@ -3,16 +3,15 @@ from typing import Optional
 from pydantic import UUID4, BaseModel, Field
 
 
-class ProjectBase(BaseModel):
+class ProjectShow(BaseModel):
     name: str = Field(min_length=3, max_length=25)
     description: Optional[str] = Field(None, max_length=120)
     author: UUID4
-
-
-class ProjectShow(ProjectBase):
     project_id: UUID4
     is_active: bool
 
 
-class ProjectCreate(ProjectBase):
-    pass
+class ProjectCreate(BaseModel):
+    name: str = Field(min_length=3, max_length=25)
+    description: Optional[str] = Field(None, max_length=120)
+    author: UUID4
