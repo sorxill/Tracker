2947f600b3da9c4b57bc6c3009aa2c8ec620ef0a
Yaroslav <yarikbritov@yandex.ru>
2024-01-09 18:29:04 +0300

    feat: create crud/schemas/dals for project

---
+++ src/api/actions/project/crud.py
@@ -0,0 +1,52 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from uuid import UUID
+
+from src.api.schemas.project import ProjectCreate, ProjectShow
+from src.db.dals.project import ProjectDAL
+from src.db.models.project import Project
+
+
+async def create_new_project(body: ProjectCreate, session: AsyncSession) -> ProjectShow:
+    async with session.begin():
+        project_dal = ProjectDAL(session)
+        project = await project_dal.dal_create_project(
+            name=body.name,
+            author=body.author,
+            description=body.description,
+        )
+        return ProjectShow(
+            name=project.name,
+            description=project.description,
+            author=project.author,
+            project_id=project.project_id,
+            is_active=project.is_active,
+        )
+
+
+async def read_project_by_name(name: str, session: AsyncSession) -> Project | None:
+    async with session.begin():
+        project_dal = ProjectDAL(session)
+        project = await project_dal.dal_get_project_by_name(name)
+        if project is not None:
+            return project
+
+
+async def delete_project(project_id, session) -> UUID | None:
+    async with session.begin():
+        project_dal = ProjectDAL(session)
+        deleted_project_id = await project_dal.dal_delete_project(
+            project_id=project_id,
+        )
+        return deleted_project_id
+
+
+async def update_project(
+    updated_project_params: dict, project_id: UUID, session
+) -> Project | None:
+    async with session.begin():
+        project_dal = ProjectDAL(session)
+        updated_project = await project_dal.dal_update_project(
+            project_id=project_id, **updated_project_params
+        )
+        return updated_project
--- src/api/schemas/project.py
+++ src/api/schemas/project.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Optional
 
 from pydantic import UUID4, BaseModel, Field
@@ -5,7 +6,7 @@ from pydantic import UUID4, BaseModel, Field
 
 class ProjectShow(BaseModel):
     name: str = Field(min_length=3, max_length=25)
-    description: Optional[str] = Field(None, max_length=120)
+    description: Optional[str] = Field(max_length=120)
     author: UUID4
     project_id: UUID4
     is_active: bool
@@ -15,3 +16,12 @@ class ProjectCreate(BaseModel):
     name: str = Field(min_length=3, max_length=25)
     description: Optional[str] = Field(None, max_length=120)
     author: UUID4
+
+
+class ProjectDelete(BaseModel):
+    project_id: UUID4
+
+
+class ProjectUpdateRequest(BaseModel):
+    name: Optional[str] = Field(min_length=3, max_length=25)
+    description: Optional[str] = Field(max_length=120)
