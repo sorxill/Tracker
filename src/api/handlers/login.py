dfd0fe731e81b9e4742e37706a6789767c91bac5
Yaroslav <yarikbritov@yandex.ru>
2024-01-21 15:05:07 +0300

    feat: update handlers for login
    refactor: refactor tag for task

--- src/api/handlers/login.py
+++ src/api/handlers/login.py
@@ -1,49 +1,35 @@
 from logging import getLogger
 
-from fastapi import APIRouter, Depends, Form, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
-from starlette import status
+from fastapi import APIRouter, Depends
 
-from src.api.actions.user.crud import read_user_by_email
+from src.api.handlers.login_utils import validate_auth_user, get_current_auth_user
 from src.api.schemas.token import TokenInfo
 from src.api.schemas.user import UserForToken
-from src.auth.hasher import Hasher
 from src.auth.jwt import JWT
-from src.db.session import get_db
 
 logger = getLogger(__name__)
 
 login_router = APIRouter(prefix="/login", tags=["login"])
 
 
-async def validate_auth_user(
-    email: str = Form(),
-    password: str = Form(),
-    db: AsyncSession = Depends(get_db),
-):
-    unauthorized_exc = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="invalid email or password",
-    )
-    user = await read_user_by_email(email, db)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with {email} not found",
-        )
-    if not Hasher.validate_password(password, hashed_password=user.hashed_password):
-        raise unauthorized_exc
-    return user
-
-
-@login_router.post("/", response_model=TokenInfo)
+@login_router.post("/auth", response_model=TokenInfo)
 async def auth_user_jwt(user: UserForToken = Depends(validate_auth_user)):
     jwt_payload = {
-        "sub": user.email,
-        "name": user.name,
+        "email": user.email,
+        "user_id": str(user.user_id),
     }
     token = JWT.encode_jwt(jwt_payload)
     return TokenInfo(
         access_token=token,
         type_token="Bearer",
     )
+
+
+@login_router.get("/check_user")
+async def auth_check_user_info(
+    user: UserForToken = Depends(get_current_auth_user),
+):
+    return {
+        "email": user.email,
+        "user_id": user.user_id,
+    }
---
+++ src/api/handlers/login_utils.py
@@ -0,0 +1,75 @@
+from fastapi import Depends, Form, HTTPException
+from jwt import InvalidTokenError
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from fastapi.security import HTTPBearer, OAuth2PasswordBearer
+
+from src.api.actions.user.crud import read_user_by_email
+from src.api.schemas.user import UserForToken
+from src.auth.hasher import Hasher
+from src.db.session import get_db
+
+from src.auth.jwt import JWT
+
+
+http_bearer = HTTPBearer()
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/api/login/auth",
+)
+
+
+async def validate_auth_user(
+    username: str = Form(),
+    password: str = Form(),
+    db: AsyncSession = Depends(get_db),
+):
+    unauthorized_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="invalid email or password",
+    )
+    user = await read_user_by_email(
+        email=username,
+        session=db,
+    )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with {username} not found",
+        )
+    if not Hasher.validate_password(
+        password,
+        hashed_password=user.hashed_password,
+    ):
+        raise unauthorized_exc
+    return user
+
+
+async def get_current_token_payload(
+    token: str = Depends(oauth2_scheme),
+) -> dict:
+    try:
+        payload = JWT.decode_jwt(
+            token=token,
+        )
+    except InvalidTokenError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"invalid token error: {e}",
+        )
+    return payload
+
+
+async def get_current_auth_user(
+    payload: dict = Depends(get_current_token_payload),
+    db: AsyncSession = Depends(get_db),
+) -> UserForToken:
+    email = payload.get("email")
+    user_db = await read_user_by_email(email, db)
+    if user_db:
+        return UserForToken(email=user_db.email, user_id=user_db.user_id)
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="token invalid (user not found)",
+    )
