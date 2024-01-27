090909a4728e4e31496ed69ba1cb612ffb12fc8f
Yaroslav <yarikbritov@yandex.ru>
2024-01-27 21:06:15 +0300

    feat: add schemas for tasks/update old schemas

--- src/api/schemas/task.py
+++ src/api/schemas/task.py
@@ -13,12 +13,21 @@ class TaskTypeEnum(str, Enum):
     MILESTONE = "milestone"
 
 
+class TaskStatusEnum(str, Enum):
+    BACKLOG = "backlog"
+    IN_PROGRESS = "in_progress"
+    READY = "ready"
+    ON_REVIEW = "on_review"
+    DONE = "done"
+
+
 class TaskShow(BaseModel):
     task_id: UUID4
-    project: UUID4
-    author: UUID4
+    project_id: UUID4
+    author_id: UUID4
     name: str = Field(min_length=3, max_length=35)
     task_type: TaskTypeEnum
+    task_status: TaskStatusEnum
     # collaborators: type[set[UUID4]] = conset(UUID4, min_length=1)
     collaborators: UUID4
     description: Optional[str] = Field(max_length=250, default=None)
@@ -30,6 +39,7 @@ class TaskCreate(BaseModel):
     author: UUID4
     name: str = Field(min_length=3, max_length=35)
     task_type: TaskTypeEnum
+    task_status: TaskStatusEnum
     # collaborators: type[set[UUID4]] = conset(UUID4, min_length=1)
     collaborators: UUID4
     description: Optional[str] = Field(max_length=250, default=None)
@@ -49,7 +59,7 @@ class TaskCreate(BaseModel):
 class TaskUpdate(BaseModel):
     name: Optional[str] = Field(min_length=3, max_length=35, default=None)
     task_type: Optional[TaskTypeEnum] = None
-    collaborators: UUID4
+    collaborators: Optional[UUID4] = None
     description: Optional[str] = Field(max_length=250, default=None)
     timestamp: Optional[datetime] = None
 
@@ -61,6 +71,10 @@ class TaskUpdate(BaseModel):
                 detail="Timestamp is only available for task_type 'milestone'",
             )
         return timestamp
+
+
+class TaskUpdateStatus(BaseModel):
+    task_status: TaskStatusEnum
 
 
 class TaskDelete(BaseModel):
