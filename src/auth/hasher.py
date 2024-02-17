e1daf915d2e06aba2b4248d6611eec83dd8ea776
Yaroslav <yarikbritov@yandex.ru>
2024-02-17 16:20:33 +0300

    docs: add docs comments to auth

--- src/auth/hasher.py
+++ src/auth/hasher.py
@@ -1,11 +1,24 @@
+"""
+Hasher to hash and validate password
+"""
+
 import bcrypt
 
 
 class Hasher:
+    """
+    Hasher class to hash and validate password
+    With salt to crypt and check gor validate
+    """
+
     @staticmethod
     def hash_password(
         password: str,
     ) -> bytes:
+        """
+        Return a hashed password by salt
+        """
+
         salt = bcrypt.gensalt()
         pwd_bytes: bytes = password.encode(encoding="utf-8")
         return bcrypt.hashpw(pwd_bytes, salt)
@@ -15,6 +28,10 @@ class Hasher:
         password: str,
         hashed_password: bytes,
     ) -> bool:
+        """
+        Return validate password
+        """
+
         return bcrypt.checkpw(
             password=password.encode(),
             hashed_password=hashed_password,
